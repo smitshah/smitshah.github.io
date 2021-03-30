26d517cbe4f237fa7aff34097dae8f6b384b7880
Smit Shah <smitshah0885@gmail.com>
2021-03-30 10:12:40 +0530

    Update contact-form-process.php

--- assets/php/contact-form-process.php
+++ assets/php/contact-form-process.php
@@ -2,8 +2,8 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "New form submissions";
+    $email_to = "smitshah0885@.com";
+    $email_subject = "New form submissions from Personal Portfolio";
 
     function problem($error)
     {
